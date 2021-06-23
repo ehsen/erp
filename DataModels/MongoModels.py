df82ae9e2df524f5e55a8ffee7e531d64e0aa57a
Ehsen Siraj <ehsensiraj@gmail.com>
2021-06-23 13:36:16 +0500

    MongoModels Updated With New

--- DataModels/MongoModels.py
+++ DataModels/MongoModels.py
@@ -1,6 +1,7 @@
 from mongoengine.fields import *
-from mongoengine import Document
+from mongoengine import Document,connect
 from Kernel.AccTypes import AccType,AccGroup,Side
+from Kernel.dummy_data import list_data
 
 """
 THIS IS THE STARTING POINT OF ERP. If you are reading code start from here
@@ -14,18 +15,19 @@ Problem is, programmers make ERP complicated. Being an accountant, I would try t
 using just simple accounting constructs, because I have plenty of stuff of worry about than just 
 doing kung fu with python
 
-AT THE MOMENT I WILL FOCUS ONLY ON TAX COMPLAINCE OF PAKISTAN and IFRS.
+AT THE MOMENT I WILL FOCUS ONLY ON TAX COMPLIANCE OF PAKISTAN and IFRS.
 """
 
 class Account(Document):
     doc_id = SequenceField(unique=True)
 
-    acc_group = EnumField(AccGroup)
-    acc_type = EnumField(AccType)
+    acc_group = StringField(required=True)
+    acc_type = StringField(required=True)
     name = StringField(max_length=30)
     tax_code = StringField()
     ifrs_code = StringField()
     tags = DictField()
+    meta = {'allow_inheritance':True}
 
 
 
@@ -34,25 +36,34 @@ class TransactionModel(Document):
     This class defines structure of Transaction in database. account field should be a reference. But I want to
     avoid refrences. I can ensure the accuracy of account names seperately.
     """
-    doc_id = SequenceField(unique=True)
+    doc_id = IntField(unique=True)
     uuid = UUIDField(primary_key=True,unique_with='doc_id')
-    date_time = DateTimeField()
-    side = EnumField(Side)
-    account = StringField()
-    amount = FloatField()
+    trans_type = StringField(required=True)
+    date_time = DateTimeField(required=True)
+    side = EnumField(Side,required=True)
+    account = StringField(required=True)
+    amount = FloatField(required=True)
     round_off_diff = FloatField()
 
 class Item(Document):
-    doc_id = StringField(unique=True)
+    doc_id = IntField(unique=True,required=True)
     item_code = StringField(max_length=30,unique=True)
-    name = StringField(max_length=30)
-    uuid = UUIDField(binary=False,unique=True)
+    name = StringField(max_length=30,required=True)
+    inventory_type = StringField(required=True)
+    uuid = UUIDField(unique=True,required=True)
 
-#x=Account(code = 1,name="Rent Account")
-#x.save()
+class InventoryTypes(Document):
+    uuid = UUIDField(required=True)
+    name = StringField(max_length=30,unique=True)
 
-"""
-for acc in Account.objects(acc_type = "Expense"):
-    print(acc.name)
-    print(type(acc.acc_group.value))
-"""
+
+class TransSeq(Document):
+
+    doc_id = SequenceField(unique=True)
+
+class ItemSeq(Document):
+    doc_id = SequenceField(unique=True)
+
+
+
+
